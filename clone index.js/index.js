7b51ab8ca8bc77b2cb07ac938bc14760dc6b87a8
AndalesJ <163089246+AndalesJ@users.noreply.github.com>
2024-05-14 13:13:18 +0800

    Add files via upload
    checking

---
+++ clone index.js/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+const profile = {
+    education: [
+        degree - 'Science, Technology, Engineering, and Mathematics',
+        year - '2016-2020',
+        university - 'University of Southern Philippines Foundation',
+
+        degree - 'Bachelor of Science in Information Technology',
+        year - '2022-2024',
+        university - 'University of Southern Philippines Foundation'
+        
+    ],
+    skills:[
+        'Proficient in typing skills and speed.',
+        'Basic understanding of front-end web development.'
+    ],
+    experience:[
+        'Airbnb - Customer Service (2024): Responding to customer concerns online.',
+        'Basic frontend web designer.'
+    ],
+    awards: [
+        'Deans Lister (2023)'
+    ],
+    personalInfo: {
+        name: "Josh Bennedick A. Andales",
+        address: "3787 J. Labra St., Guadalupe, Cebu City, Philippines",
+        phonenumber: "09761945795",
+        email: "joshandales03@gmail.com",
+        birthdate: "September 15, 2003",
+        sex: "Male"
+    }
+};
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json(profile);
+});
+
+app.listen(port, () => {
+    console.log(`Server running om http://localhost:${port}`);
+});
